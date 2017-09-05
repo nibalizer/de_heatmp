41d6a8091698272ee6d65e5428882523c7749c17
Cullen Taylor <cullentaylor@outlook.com>
2017-09-05 16:54:53 -0500

    [WIP] Donde esta la biblioteca
    Signed-off-by: Cullen Taylor <cullentaylor@outlook.com>

--- cheeseshop_heatmap.js
+++ cheeseshop_heatmap.js
@@ -1,91 +1,31 @@
-// Don't know what this does
-window.requestAnimationFrame = window.requestAnimationFrame ||
-                               window.mozRequestAnimationFrame ||
-                               window.webkitRequestAnimationFrame ||
-                               window.msRequestAnimationFrame;
+let csgoMaps = require("./csgo_maps.js");
 
-// Setup websocket connection
-var connection = new WebSocket('ws://localhost:5051/games/csgo/gsi/sources/032ea9b5-867f-462f-98a7-5c415272ceee/play');
+function Heatmap(mapName, elementId) {
+  // create the heatmap
+  var null_data = [];
+  this.heat = simpleheat(elementId).data(null_data).max(18), frame;
+  heat.radius(15, 10);
 
-connection.onopen = function(){
-  /*Send a small message to the console once the connection is established */
-  console.log('Connection open!');
-}
+  // get csgo map data
+  var mapData = csgoMaps.mapDetails[mapName];
 
-
-// simpleheat boilerplate
-function get(id) {
-    return document.getElementById(id);
-}
-var null_data = [];
-
-
-// create the heatmap
-var heat = simpleheat('canvas').data(null_data).max(18),
-    frame;
-heat.radius(15, 10);
-
-
-function draw() {
-    //console.time('draw');
+  this.draw = function() {
     heat.draw();
-    //console.timeEnd('draw');
     frame = null;
-}
+  };
 
-// end simpleheat boilerplate
-// translate notes
-// 2263 coordinates = 422 pixels
+  this.get = function (id) {
+      return document.getElementById(id);
+  }
 
-var map_name = "unset";
+  this.translate_coordinates = function (x_game, y_game){
+    pos_x = mapData.pos_x;
+    pos_y = mapData.pos_y;
+    scale_factor = 6.scale;
 
-var translate_coordinates_cache = function (x_game, y_game){
-  // upper left corner = -2000, 3250
-  // scale factor = 5.5
-  pos_x = -3840;
-  pos_y = 3072;
-  scale_factor = 6;
+    x_prime = (x_game - pos_x) / scale_factor;
+    y_prime = (pos_y - y_game) / scale_factor;
 
-  x_prime = (x_game - pos_x) / scale_factor;
-  y_prime = (pos_y - y_game) / scale_factor;
-
-
-  return { "x": x_prime, "y":  y_prime};
-}
-
-// respond to websocket messages
-connection.onmessage = function(msg){
-  //console.log("message recieved, captian");
-  payload = JSON.parse(msg.data);
-  if ("allplayers" in payload){
-    // get positional data points, draw
-
-    Object.keys(payload.allplayers).forEach ( function(player_id, index) {
-      console.log("player id: " + player_id);
-      // get position of player  - translate into heatmap coordinates - display
-      position = payload.allplayers[player_id].position.split(",").map(parseFloat);
-      console.log(payload.allplayers[player_id].name + " " + payload.allplayers[player_id].team );
-      console.log(position);
-      updated_position = (translate_coordinates_cache(position[0], position[1]));
-      console.log(updated_position);
-      heat.add([updated_position["x"], updated_position["y"], 2]);
-      frame = frame || window.requestAnimationFrame(draw);
-    });
-
-
+    return {"x": x_prime, "y": y_prime};
   }
 }
-
-// Draw heatmap
-draw();
-
-// simpleheat stuff, needs to be after draw() (??)
-var radius = get('radius'),
-    blur = get('blur'),
-    changeType = 'oninput' in radius ? 'oninput' : 'onchange';
-
-radius[changeType] = blur[changeType] = function (e) {
-    heat.radius(+radius.value, +blur.value);
-    frame = frame || window.requestAnimationFrame(draw);
-};
-
