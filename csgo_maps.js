0c47a28ebc79e9eefd6cbad08198088b675c5b5e
Spencer Krum <nibz@spencerkrum.com>
2017-09-05 14:22:21 -0500

    Add csgo_maps data file

---
+++ csgo_maps.js
@@ -0,0 +1,12 @@
+var map_details = {
+  "de_cbble": {
+    "pos_x" : "-3840", // upper left world x coordinate
+    "pos_y" : "3072", // upper left world y coordinate
+    "scale" : "6"
+  }
+  "": {
+    "pos_x" : "-3840", // upper left world x coordinate
+    "pos_y" : "3072", // upper left world y coordinate
+    "scale" : "6"
+  }
+
